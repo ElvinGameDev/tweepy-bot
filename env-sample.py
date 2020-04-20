cb04ceaaefea51438e9a35ebc2311c236f5a951d
akihisaSakai <akihisa.sakai@soarhap.com>
2020-04-20 21:48:35 +0900

    update: adjust envrion

--- env-sample.py
+++ env-sample.py
@@ -1,7 +1,7 @@
 import os
 
 def load_env():
-  os.environ['CONSUMER_KEY'] = 'CONSUMER_KEY'
-  os.environ['CONSUMER_SECRET'] = 'CONSUMER_SECRET'
-  os.environ['ACCESS_TOKEN'] = 'ACCESS_TOKEN'
-  os.environ['ACCESS_TOKEN_SECRET'] = 'ACCESS_TOKEN_SECRET'
+  os.environ["CONSUMER_KEY"] = "CONSUMER_KEY"
+  os.environ["CONSUMER_SECRET"] = "CONSUMER_SECRET"
+  os.environ["ACCESS_TOKEN"] = "ACCESS_TOKEN"
+  os.environ["ACCESS_TOKEN_SECRET"] = "ACCESS_TOKEN_SECRET"
--- index.py
+++ index.py
@@ -1,22 +1,23 @@
 import os
-import random
-from env import load_env
+from env import set_env
+
+# set environ
 from pkg.tweepyHandler import TweepyHandler
+set_env()
 
 # init & auth
-load_env()
 th = TweepyHandler(
-    os.environ['CONSUMER_KEY'],
-    os.environ['CONSUMER_SECRET'],
-    os.environ['ACCESS_TOKEN'],
-    os.environ['ACCESS_TOKEN_SECRET']
+    os.environ["CONSUMER_KEY"],
+    os.environ["CONSUMER_SECRET"],
+    os.environ["ACCESS_TOKEN"],
+    os.environ["ACCESS_TOKEN_SECRET"]
 )
 
-# search
+# search & get tweets
 th.search_add_to_tweets(query="#myFirstTweet", result_type="recent", count=20)
 th.search_add_to_tweets(query="#100DaysOfCode", result_type="recent", count=40)
 
-# like
+# action
 for seached_tweet in th.tweets:
     try:
         th.do_like_to_tweet(seached_tweet)
